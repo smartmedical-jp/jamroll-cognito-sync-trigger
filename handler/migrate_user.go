13384400af89175f7d3d97a0b683d31edb71fa2e
Masaki Arai <mkosakana.dev@gmail.com>
2024-10-10 12:13:34 +0900

    コメント追加

--- handler/migrate_user.go
+++ handler/migrate_user.go
@@ -27,7 +27,15 @@ func MigrateUserHandler(event events.CognitoEventUserPoolsMigrateUser) (events.C
 		if err != nil {
 			return event, err
 		}
+	// パスワードリセットがリクエストされ、そのリクエストユーザがユーザプールに存在しない場合のみ、このトリガーが発生する
+	// ・リクエスト時点では、パスワードリセット対象メールアドレスだけ送信される
+	// ・新パスワードを設定するためのUIは別途任意のタイミングで表示必要がある
 	case TriggerSourceForgotPassword:
+		// TODO 1. Firebase に存在するか確認
+		// TODO 2. Firebase に存在する場合、新パスワードを設定するためのUIを表示
+		// 存在しない場合は、エラーを返す？
+		// TODO 3. Firebase でパスワードリセット処理を行う（何かあって Firebase にまた戻す、みたいな状況を考慮（要らんかも））
+		// TODO 4. Firebase でパスワードリセット処理が成功したら、ユーザープールに メールアドレス + 新パスワード でユーザを作成
 	}
 
 	return event, nil
