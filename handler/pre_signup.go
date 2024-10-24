9e4fc2cdb173ed0f6a88d31ac9957c9be3e21596
Masaki Arai <mkosakana.dev@gmail.com>
2024-10-24 20:09:48 +0900

    Add check existing user

--- handler/pre_signup.go
+++ handler/pre_signup.go
@@ -49,13 +49,20 @@ func PreSignupHandler(
 		・直接CDKなどでこの操作を行う
 		場合にトリガされる
 
-		移行トリガーはユーザプールにログインユーザが存在しない場合にのみトリガされるので、既存ユーザとの重複チェックは不要
-		また、SOLID にならないが、migrate_user コントローラー（移行トリガー発火時の実装）で firebase に存在するかチェックしているためここではチェックしない
+		認証方法が違うと、メールアドレスが同じでも異なるユーザとして扱われてしまうため、重複するユーザが存在する場合エラーにする
+		SOLID にならないが、migrate_user コントローラー（移行トリガー発火時の実装）で firebase に存在するかチェックしているためここではチェックしない
 		（※ 米国のデータストアに毎回確認しにいく firebase 処理を行う回数を減らすことを優先）
 
-		さらに、現状（2024/10/09）機能開発のなかでそれが使用される可能性は低いので後者も無視
+		また、現状（2024/10/09）機能開発のなかでそれが使用される可能性は低いので後者も無視
 	*/
 	case TriggerSourceAdminCreateUser:
+		exist, err := cognito.ExistByEmail(email)
+		if err != nil {
+			return event, err
+		}
+		if exist {
+			return event, cognito.ErrUserAlreadyExist
+		}
 
 	/*
 		ソーシャルログインが成功し、そのユーザが Cognito ユーザプールに存在しない場合に、そのユーザを登録しようとしてトリガされる
