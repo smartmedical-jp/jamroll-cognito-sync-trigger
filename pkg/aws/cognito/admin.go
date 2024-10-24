6ac6d51c60d8dfc688ee5067c97c948018b1bb43
Masaki Arai <mkosakana.dev@gmail.com>
2024-10-24 19:50:38 +0900

    Mod use sourceUserName

--- pkg/aws/cognito/admin.go
+++ pkg/aws/cognito/admin.go
@@ -1,10 +1,12 @@
 package cognito
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"jam-roll-cognito-sync-trigger/pkg/aws/setting"
+	"strings"
 )
 
 // AdminLinkUser Cognito では認証方法が違うとメールアドレスが同じでも異なるユーザとして扱われてしまうため、統合する
@@ -19,15 +21,18 @@ func AdminLinkUser(
 	}
 
 	// https://docs.aws.amazon.com/ja_jp/cognito-user-identity-pools/latest/APIReference/API_AdminLinkProviderForUser.html
-	_, err = client.AdminLinkProviderForUser(&cognitoidentityprovider.AdminLinkProviderForUserInput{
+	sourceUserName := strings.Split(event.UserName, "_")[0]
+	if sourceUserName == "" {
+		return fmt.Errorf("sourceUserName is empty")
+	}
+	res, err := client.AdminLinkProviderForUser(&cognitoidentityprovider.AdminLinkProviderForUserInput{
 		DestinationUser: &cognitoidentityprovider.ProviderUserIdentifierType{
 			ProviderAttributeValue: aws.String(*destinationUser.Username),
 			ProviderName:           aws.String("Cognito"),
 		},
 		SourceUser: &cognitoidentityprovider.ProviderUserIdentifierType{
-			ProviderAttributeName: aws.String("Cognito_Subject"),
-			// TODO 上書きされるのか確認（多分されそう）
-			ProviderAttributeValue: aws.String(event.UserName),
+			ProviderAttributeName:  aws.String("Cognito_Subject"),
+			ProviderAttributeValue: aws.String(sourceUserName),
 			ProviderName:           aws.String(provider.String()),
 		},
 		UserPoolId: aws.String(setting.GetUserPoolID()),
@@ -35,6 +40,7 @@ func AdminLinkUser(
 	if err != nil {
 		return err
 	}
+	fmt.Println(res)
 
 	return nil
 }
